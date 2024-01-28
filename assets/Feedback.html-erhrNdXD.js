edd51e0a720fd906de3cb9b87f90ededada27b99
HTHou <hhaonan@outlook.com>
2024-01-28 17:06:39 +0800

    Site checkin for project tsfile-website

---
+++ assets/Feedback.html-erhrNdXD.js
@@ -0,0 +1,20 @@
+import{_ as o,r as s,o as a,c as r,a as i,d as e,e as t,f as c,b as l}from"./app-IhqvkLYY.js";const h={},p=e("h2",{id:"联系我们",tabindex:"-1"},[e("a",{class:"header-anchor",href:"#联系我们","aria-hidden":"true"},"#"),t(" 联系我们")],-1),d=e("p",null,"作为 Apache 项目，我们使用邮件列表、JIRA 和 Github 的 issue 模块来获取用户反馈。",-1),u=l("<li><p>邮件列表：dev@iotdb.apache.org.</p><ul><li>该列表同时用于用户反馈和开发者交流。</li><li>订阅方法：用想接收邮件的邮箱向 dev-subscribe@iotdb.apache.org 发一封邮件，主题内容不限，收到回复后， 再次向确认地址发一封确认邮件（确认地址比较长，推荐 qq 邮箱）。</li></ul></li><li><p>JIRA issue: https://issues.apache.org/jira/projects/IOTDB/issues</p></li><li><p>Github issue: https://github.com/apache/iotdb/issues</p></li><li><p>Twitter: https://twitter.com/apacheiotdb</p></li><li><p>对中文用户，欢迎加入 QQ 群：659990460.</p></li>",5),_={href:"https://github.com/apache/iotdb/issues/748",target:"_blank",rel:"noopener noreferrer"},m=e("li",null,[e("p",null,"对中文用户，欢迎关注微信公众号，IoTDB 漫游指南")],-1),b=e("li",null,[e("p",null,"对中文用户，我们提供了钉钉群，可以搜索群名称加入：Apache IoTDB 交流 1 群")],-1);function f(g,I){const n=s("ExternalLinkIcon");return a(),r("div",null,[i(`
+
+    Licensed to the Apache Software Foundation (ASF) under one
+    or more contributor license agreements.  See the NOTICE file
+    distributed with this work for additional information
+    regarding copyright ownership.  The ASF licenses this file
+    to you under the Apache License, Version 2.0 (the
+    "License"); you may not use this file except in compliance
+    with the License.  You may obtain a copy of the License at
+    
+        http://www.apache.org/licenses/LICENSE-2.0
+    
+    Unless required by applicable law or agreed to in writing,
+    software distributed under the License is distributed on an
+    "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+    KIND, either express or implied.  See the License for the
+    specific language governing permissions and limitations
+    under the License.
+
+`),p,d,e("ul",null,[u,e("li",null,[e("p",null,[t("我们非常期待您分享您使用 IoTDB 的经验："),e("a",_,[t("调研问卷"),c(n)])])]),m,b]),i(" IoTDB 社区会不定期举办 Meetup，相关信息会在邮件列表和 [研讨会议和报告列表](https://iotdb.apache.org/Community/Materials.html) 页面找到。 ")])}const A=o(h,[["render",f],["__file","Feedback.html.vue"]]);export{A as default};
